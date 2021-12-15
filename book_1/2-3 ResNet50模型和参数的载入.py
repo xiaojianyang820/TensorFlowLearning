483506ff444ebec36fe3032667d9e2d4888e6a2e
茂陵侯 <980890611@qq.com>
2021-12-15 10:18:51 +0800

    4

---
+++ book_1/2-3 ResNet50模型和参数的载入.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+
+resnet_layer = tf.keras.applications.resnet50.ResNet50(weights='imagenet', include_top=False, pooling=False)
+resnet_layer.trainable = False
+flatten_layer = tf.keras.layers.GlobalAveragePooling2D()
+drop_out_layer = tf.keras.layers.Dropout(0.1)
+fc_layer = tf.keras.layers.Dense(2)
+
+binary_classes = tf.keras.Sequential(
+    [resnet_layer, flatten_layer, drop_out_layer, fc_layer]
+)
+print(binary_classes.summary())
